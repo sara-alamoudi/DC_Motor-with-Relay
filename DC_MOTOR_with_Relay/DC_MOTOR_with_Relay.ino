21e5d786324c57ed9d7599d1d77accc064c5c087
sara-alamoudi <66751581+sara-alamoudi@users.noreply.github.com>
2020-06-23 21:01:59 +0300

    Add files via upload

---
+++ DC_MOTOR_with_Relay/DC_MOTOR_with_Relay.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+
+}
+
+void loop()
+{
+  
+  digitalWrite(13,HIGH);
+  delay(2000);
+  digitalWrite(13,LOW);
+  delay(2000);
+  digitalWrite(12,HIGH);
+  delay(2000);
+  digitalWrite(12,LOW);
+  delay(2000);
+
+}
